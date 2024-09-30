433ec6ecc10b309b55721e2f73440edcf76d5271
mahmoud <mahmoodkhatib@hotmail.com>
2024-09-30 17:39:36 +0300

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
-from .database import engine
 from .routers import users, posts, auth, votes
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+origins = [
+    "*" # All domains are not blocked
+    # "https://www.google.com",
+    # "https://www.youtube.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
